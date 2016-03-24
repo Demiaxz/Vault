8cab0f18405b8cd540a6aeca76303e7441e7b102
Kevin Smits <demiaxz@hotmail.com>
2016-03-24 16:13:50 +0100

    Mooie kleurtjessss

--- app/src/main/java/nl/hsleiden/vault/vault/List/CourseListActivity.java
+++ app/src/main/java/nl/hsleiden/vault/vault/List/CourseListActivity.java
@@ -1,8 +1,6 @@
 package nl.hsleiden.vault.vault.List;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -25,14 +23,6 @@ public class CourseListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courselist);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-            }
-        });
 
         mListView = (ListView) findViewById(R.id.my_list_view);
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
