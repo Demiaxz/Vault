0dcb9e21fd3c8c5ee000ef19c27278d88cc530d8
Kevin Smits <demiaxz@hotmail.com>
2016-04-13 21:54:38 +0200

    SettingsBox

--- app/src/main/java/nl/hsleiden/vault/vault/fragments/SettingsAlerter.java
+++ app/src/main/java/nl/hsleiden/vault/vault/fragments/SettingsAlerter.java
@@ -8,27 +8,55 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ListView;
 
+import nl.hsleiden.vault.vault.Database.PairValue;
 import nl.hsleiden.vault.vault.R;
 
 /**
  * Created by Perseus on 06-04-16.
  */
+
 public class SettingsAlerter extends DialogFragment {
+    private String toChange = null;
+    private String oldInput = null;
+    private ListView view = null;
+    private PairValue pv = null;
+    private menuInfoListAdapter blist = null;
+
+    public SettingsAlerter(){
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         LayoutInflater inflater = (LayoutInflater)getActivity().getSystemService (Context.LAYOUT_INFLATER_SERVICE);
-        View v = inflater.inflate(R.layout.content_settings_alerter, null);
+        final View v = inflater.inflate(R.layout.content_settings_alerter, null);
 
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setMessage("Settings")
                 .setPositiveButton("Done", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
+
+                        CheckBox tv1 = (CheckBox) (v.findViewById(R.id.checkBox));
+                        CheckBox tv2 = (CheckBox) (v.findViewById(R.id.checkBox2));
+
+                        if (tv1.isChecked()) {
+                            //only sync no push
+                            v.getContext().getSharedPreferences("userData", 0).edit().putBoolean("update", true).commit();
+                            if (tv2.isChecked()) {
+                                //also push
+                                v.getContext().getSharedPreferences("userData", 0).edit().putBoolean("push", true).commit();
+                            }
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
                         // FIRE ZE MISSILES!
                     }
                 });
-
 
         builder.setView(v);
 
