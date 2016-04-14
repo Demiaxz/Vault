913bd1b3c43839951011916ceace1957d57452c7
Kevin Smits <demiaxz@hotmail.com>
2016-04-14 14:16:03 +0200

    added Course attribute

--- app/src/main/java/nl/hsleiden/vault/vault/Database/DatabaseInfo.java
+++ app/src/main/java/nl/hsleiden/vault/vault/Database/DatabaseInfo.java
@@ -10,6 +10,7 @@ public class DatabaseInfo {
     }
 
     public class CourseColumn {
+        public static final String ID = "ID";
         public static final String NAME = "name";        // VASTE WAARDES
         public static final String ECTS = "ects";        // NAAM VAN DE KOLOMMEN
         public static final String GRADE = "grade";           // CIJFER
