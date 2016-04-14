fa2dc3c518b41cda00ad15bffbdec7719f5f65e2
Kevin Smits <demiaxz@hotmail.com>
2016-04-14 14:08:51 +0200

    added Course attributes

--- app/src/main/java/nl/hsleiden/vault/vault/Database/Course.java
+++ app/src/main/java/nl/hsleiden/vault/vault/Database/Course.java
@@ -8,7 +8,14 @@ public class Course implements Serializable {           // WAAROM serializable ?
     private String ects;
     private String grade;
     private String period;
+    private String testdate;
+    private String description;
+    private String impact;
+    private String concept;
+    private String mutationdate;
+    private String core;
     private String story;
+    private String ID;
 
     public Course(String courseName, String ects, String grade, String period, String story){
         this.name = courseName;
@@ -16,6 +23,19 @@ public class Course implements Serializable {           // WAAROM serializable ?
         this.grade = grade;
         this.period = period;
         this.story = story;
+    }
+
+    public Course (String name, String ects, String grade, String period, String testdate, String description, String impact, String concept, String mutationdate, String core){
+        this.name = name;
+        this.ects = ects;
+        this.grade = grade;
+        this.period = period;
+        this.testdate = testdate;
+        this.description = description;
+        this.impact = impact;
+        this.concept = concept;
+        this.mutationdate = mutationdate;
+        this.core = core;
     }
 
     //Getters
@@ -31,7 +51,22 @@ public class Course implements Serializable {           // WAAROM serializable ?
     public String getPeriod() {
         return period;
     }
-
+    public String getTestdate() {
+        return testdate;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public String getImpact() {
+        return impact;
+    }
+    public String getConcept() {
+        return concept;
+    }
+    public String getMutationdate() {
+        return mutationdate;
+    }
+    public String getCore() {return core;}
     public String getStory() {
         return story;
     }
@@ -50,7 +85,39 @@ public class Course implements Serializable {           // WAAROM serializable ?
         this.period = period;
     }
 
+    public void setTestdate(String testdate) {
+        this.testdate = testdate;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setImpact(String impact) {
+        this.impact = impact;
+    }
+
+    public void setConcept(String concept) {
+        this.concept = concept;
+    }
+
+    public void setMutationdate(String mutationdate) {
+        this.mutationdate = mutationdate;
+    }
+
+    public void setCore(String core) {
+        this.core = core;
+    }
+
     public void setStory(String story) {
         this.story = story;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
     }
 }
